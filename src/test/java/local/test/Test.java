a4ada7fc23faca19bf3424f9bc8b4bb4d3b9db21
Thiago <thiago.lima@bommercatto.com.br>
2014-01-15 12:22:54 -0200

    test conection

---
+++ src/test/java/local/test/Test.java
@@ -0,0 +1,20 @@
+package local.test;
+
+import com.google.code.magja.magento.ConnectionTest;
+
+public class Test {
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		ConnectionTest conn = new ConnectionTest();
+		
+		conn.testProductType();
+
+	}
+
+}
